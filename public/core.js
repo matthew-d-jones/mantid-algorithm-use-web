bdf2f7c5292d1479442fef8033f10d0bc4a85fd1
matthew.d.jones <matthew.d.jones@tessella.com>
2016-07-16 14:57:08 +0100

    add a spinner which is displayed whilst data is being collected from remote server

--- public/core.js
+++ public/core.js
@@ -1,10 +1,13 @@
 // public/core.js
-var mantidAlgs = angular.module('mantidAlgs', []);
+var mantidAlgs = angular.module('mantidAlgs', ['angularSpinner']);
 
 function mainController($scope, $http) {
 
+  $scope.busy = true;
+
   $http.get('/api/records')
         .success(function(data) {
+          $scope.busy = false;
           $('#algorithms-table').dynatable({
             dataset: {
               records: data,

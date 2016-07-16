3f7e749c409133e62a9a92ddacd7cde16a389db5
matthew.d.jones <matthew.d.jones@tessella.com>
2016-07-16 15:04:33 +0100

    remove require extend module

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
       var morgan = require('morgan');             // log requests to the console (express4)
       var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
       var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-      var extend = require('extend');
 
     // configuration =================
 
